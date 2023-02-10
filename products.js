ee23fd99b561bfc63a0d1542451abeb9e93db7bd
Joe <97600250+WebXoX@users.noreply.github.com>
2023-02-10 10:08:19 +0400

    final

---
+++ products.js
@@ -0,0 +1,48 @@
+const products =[
+    {
+        id: 1001,
+        title: "MATH",
+        location: "London",
+        quantity: 10,
+        price:  300,
+        image: "image/math.png",
+        rating: 3
+    },
+     {
+        id: 1002,
+        title: "Science",
+        location: "London",
+        quantity: 10,
+        price:  100,
+        image: "image/math.png",
+        rating: 3
+    },
+     {
+        id: 1003,
+        title: "SST",
+        location: "London",
+        quantity: 10,
+        price:  20,
+        image: "image/math.png",
+        rating: 3
+    },
+    {
+        id: 1004,
+        title: "Mobile",
+        location: "London",
+        quantity: 10,
+        price:  10,
+        image: "image/math.png",
+        rating: 3
+    },
+    {
+        id: 1005,
+        title: "web",
+        location: "London",
+        quantity: 10,
+        price:  2,
+        image: "image/math.png",
+        rating: 3
+    }
+
+]
